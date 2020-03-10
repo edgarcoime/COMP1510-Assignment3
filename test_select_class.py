d2df2c646232f8665b71205c264dcd79d4a29a57
Tommy-81321 <56455799+Tommy-81321@users.noreply.github.com>
2020-03-09 18:04:14 -0700

    New edit: fix the class race unitest

--- test_select_class.py
+++ test_select_class.py
@@ -8,7 +8,7 @@ import sud
 
 
 class TestSelectClass(TestCase):
-    @unittest.mock.patch('builtins.input', side_effect=[1])
+    @unittest.mock.patch('builtins.input', side_effect=['1'])
     def test_select_class_1(self, _):
         """Test output when user input is 1"""
 
@@ -16,7 +16,7 @@ class TestSelectClass(TestCase):
         expected = "barbarian"
         self.assertEqual(actual, expected, "User input is 1")
 
-    @unittest.mock.patch('builtins.input', side_effect=[4])
+    @unittest.mock.patch('builtins.input', side_effect=['4'])
     def test_select_class_4(self, _):
         """Test output when user input is 4"""
 
@@ -24,7 +24,7 @@ class TestSelectClass(TestCase):
         expected = "druid"
         self.assertEqual(actual, expected, "User input is 4")
 
-    @unittest.mock.patch('builtins.input', side_effect=[8])
+    @unittest.mock.patch('builtins.input', side_effect=['8'])
     def test_select_class_8(self, _):
         """Test output when user input is 8"""
 
@@ -32,7 +32,7 @@ class TestSelectClass(TestCase):
         expected = "ranger"
         self.assertEqual(actual, expected, "User input is 8")
 
-    @unittest.mock.patch('builtins.input', side_effect=[12])
+    @unittest.mock.patch('builtins.input', side_effect=['12'])
     def test_select_class_12(self, _):
         """Test output when user input is 12"""
 
--- test_select_race.py
+++ test_select_race.py
@@ -8,7 +8,7 @@ import sud
 
 
 class TestSelectRace(TestCase):
-    @unittest.mock.patch('builtins.input', side_effect=[1])
+    @unittest.mock.patch('builtins.input', side_effect=['1'])
     def test_select_race_1(self, _):
         """Test output when user input is 1"""
 
@@ -16,7 +16,7 @@ class TestSelectRace(TestCase):
         expected = 'dwarf'
         self.assertEqual(actual, expected, "User input is 1")
 
-    @unittest.mock.patch('builtins.input', side_effect=[3])
+    @unittest.mock.patch('builtins.input', side_effect=['3'])
     def test_select_race_3(self, _):
         """Test output when user input is 3"""
 
@@ -24,7 +24,7 @@ class TestSelectRace(TestCase):
         expected = 'halfling'
         self.assertEqual(actual, expected, "User input is 3")
 
-    @unittest.mock.patch('builtins.input', side_effect=[6])
+    @unittest.mock.patch('builtins.input', side_effect=['6'])
     def test_select_race_6(self, _):
         """Test output when user input is 6"""
 
@@ -32,7 +32,7 @@ class TestSelectRace(TestCase):
         expected = 'gnome'
         self.assertEqual(actual, expected, "User input is 6")
 
-    @unittest.mock.patch('builtins.input', side_effect=[9])
+    @unittest.mock.patch('builtins.input', side_effect=['9'])
     def test_select_race_9(self, _):
         """Test output when user input is 9"""
 
