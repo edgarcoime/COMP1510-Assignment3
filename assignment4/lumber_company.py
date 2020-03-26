a748ef1c7aa418908a8e51b114ebc4c296c34ec4
Tommy-81321 <56455799+Tommy-81321@users.noreply.github.com>
2020-03-26 11:26:22 -0700

    New edit: fix some errors

--- assignment4/lumber_company.py
+++ assignment4/lumber_company.py
@@ -3,10 +3,7 @@ from tree import Tree
 from tree_farm import TreeFarm
 
 
-
-
-
-def add_tree(tree_farm):
+def add_tree(tree_farm: list) -> list:
     species = input("What species is the tree? ")
     age = input("What is the tree's age? ")
     circumference = input("What is the tree's circumference? ")
@@ -14,24 +11,36 @@ def add_tree(tree_farm):
         tree = Tree(species, int(age), float(circumference))
     except ValueError:
         print("You only can provide non-empty species, positive integer age and non-zero positive float circumference.")
+        return tree_farm
     else:
         tree_farm.add(tree)
         tree_farm.print_trees()
-        return tree_farm
-
-def harvest_one_tree(tree_farm):
-    diameter = input("What diameter tree would you like? ")
-    circumference = math.pi * float(diameter)
-    tree_farm.remove_tree(circumference)
-    tree_farm.print_trees()
     return tree_farm
 
 
-def harvest_some_trees(tree_farm):
-    diameter = input("What diameter trees would you like? ")
-    circumference = math.pi * float(diameter)
-    tree_farm.remove_trees(circumference)
-    tree_farm.print_trees()
+def harvest_one_tree(tree_farm: list) -> list:
+    diameter = input("What diameter tree would you like? ")
+    try:
+        circumference = math.pi * float(diameter)
+    except ValueError:
+        print("The float format only")
+        return tree_farm
+    else:
+        tree_farm.remove_tree(circumference)
+        tree_farm.print_trees()
+    return tree_farm
+
+
+def harvest_some_trees(tree_farm: list) -> list:
+    diameter = input("What diameter tree would you like? ")
+    try:
+        circumference = math.pi * float(diameter)
+    except ValueError:
+        print("The float format only")
+        return tree_farm
+    else:
+        tree_farm.remove_trees(circumference)
+        tree_farm.print_trees()
     return tree_farm
 
 
@@ -44,13 +53,13 @@ def main():
         3. Harvest some Trees
         4. Quit
         """)
-        if ask_options == str(1):
+        if ask_options.strip() == str(1):
             tree_farm = add_tree(tree_farm)
-        elif ask_options == str(2):
+        elif ask_options.strip() == str(2):
             tree_farm = harvest_one_tree(tree_farm)
-        elif ask_options == str(3):
+        elif ask_options.strip() == str(3):
             tree_farm = harvest_some_trees(tree_farm)
-        elif ask_options == str(4):
+        elif ask_options.strip() == str(4):
             break
         else:
             print("Please only type 1, 2, 3 or 4")
